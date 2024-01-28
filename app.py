cd6a8ac1475baaee813029926d1bdee60c99f2eb
Armaan Seth <seth.armaanseth@gmail.com>
2024-01-28 19:39:27 +0530

    completed api

--- app.py
+++ app.py
@@ -1,32 +1,30 @@
-import os
-from selenium import webdriver
+from flask import Flask, request
 from urllib.parse import urlparse
 from youtube_transcript_api import YouTubeTranscriptApi
 from langchain_google_genai.chat_models import ChatGoogleGenerativeAI
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.summarize import load_summarize_chain
 from langchain.docstore.document import Document
-from langchain_core.prompts import PromptTemplate
-# driver=webdriver.Chrome()
-# url=driver.current_url
 
-url="https://www.youtube.com/watch?v=mrKuDK9dGlg&t=2641s"
-url="https://www.youtube.com/watch?v=LNq_2s_H01Y"
-# url="https://www.youtube.com/watch?v=__sETDiAYqM"
-url="https://www.youtube.com/watch?v=sZZDw0i6WSA"
-vid=urlparse(url).query[2:]
+app = Flask(__name__)
+def get_summary(url):
+    if "www.youtube.com/watch" not in url:
+        return "This is not a youtube page"
+    vid=urlparse(url).query[2:]
+    transcript=YouTubeTranscriptApi.get_transcript(vid, languages=["en","hi"])
+    transcript=[t["text"] for t in transcript]
+    transcript=' '.join(transcript)
+    chunks=RecursiveCharacterTextSplitter(chunk_size=5000, chunk_overlap=0).split_text(transcript)
+    docs=[Document(page_content=t) for t in chunks]
+    llm=ChatGoogleGenerativeAI(model="gemini-pro",convert_system_message_to_human=True)
+    chain=load_summarize_chain(chain_type="stuff", llm=llm, verbose=True)
+    return chain.run(docs)
 
-transcript=YouTubeTranscriptApi.get_transcript(vid, languages=["en","hi"])
-transcript=[t["text"] for t in transcript]
-transcript=' '.join(transcript)
-chunks=RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200).split_text(transcript)
-docs=[Document(page_content=t) for t in chunks]
+@app.route('/')
+def index():
+    url = request.args.get('url', 'No URL provided')
+    res=get_summary(url)
+    return {"text":res}
 
-os.environ["GOOGLE_API_KEY"]="AIzaSyB3xjgb0DF84EvKsvKafVFdih9jpaj4jGQ"
-llm=ChatGoogleGenerativeAI(model="gemini-pro",convert_system_message_to_human=True)
-
-prompt=""""""
-
-chain=load_summarize_chain(chain_type="map_reduce", llm=llm, verbose=False, return_intermediate_steps=False)
-output_summary=chain.run(docs)
-print(output_summary)
+if __name__ == '__main__':
+    app.run(debug=True)
